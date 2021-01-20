de9168c199bfc8ce83f4033fc1e38e66a4cac58d
Panpyhr <51531508+valderpan@users.noreply.github.com>
2021-01-20 19:39:03 +0800

    Update Genome_phasing.py

--- Assembly/Genome_phasing.py
+++ Assembly/Genome_phasing.py
@@ -4,17 +4,17 @@
 # date: 2020/11/28
 
 import sys
-sys.path.append('/public1/home/stu_panhaoran/scripts/')
 import gzip
 import time
+sys.path.append('D:\pycharm\panhr\FAFU_CGB\Genomic_exercise')
 import Fontcolor
 import argparse
 
 
 __author__ = 'Haoran Pan'
 __mail__ = 'haoran_pan@qq.com'
-__date__ = '20201207'
-__version__ = 'v1.1'
+__date__ = '20200112'
+__version__ = 'v1.2'
 
 
 
@@ -45,8 +45,10 @@ def depth_count(depthfile,outputfile):
                 reads_num = 0
                 coverage_depth = 0
                 output = contig + '\t' + str(depth)
+                # print(output.rstrip())
                 result.write(output.rstrip()+'\n')
         last_depth = float(coverage_depth)/float(reads_num)
+        # print(contig+'\t'+str(last_depth))
         result.write(contig+'\t'+str(last_depth)+'\n')
     result.close()
 
@@ -74,8 +76,11 @@ def calcultate_sample_length(fq_gz_file):
             elif line_index % 4 ==2:
                 reads_length += len(line)
     end = time.time()
+    # print('File {} reads num are {}'.format(str(fq_gz_file),reads_num))
     Fontcolor.Tips_output('File {} reads num are {}'.format(str(fq_gz_file),reads_num))
+    # print('File {} reads length are {}'.format(str(fq_gz_file),reads_length))
     Fontcolor.Tips_output('File {} reads length are {}'.format(str(fq_gz_file),reads_length))
+    # print('Time cost: {:.5f}'.format(end-start))
     Fontcolor.Tips_output('Time cost: {:.5f}'.format(end-start))
     return reads_length
 
@@ -97,10 +102,18 @@ def cal_genome_size(genome_fasta):
     genome_size = 0
     for key in fa_dict.keys():
         genome_size += len(fa_dict[key])
+    # print('Genome size is {} bp'.format(genome_size))
     Fontcolor.Tips_output('Genome size is {} bp'.format(genome_size))
     return genome_size
 
 
+# def run_step2():
+#     fq1length = calcultate_std_depth2(sys.argv[1])
+#     fq2length = calcultate_std_depth2(sys.argv[2])
+#     genomesize = cal_genome_size(sys.argv[3])
+#     std_depth = round((fq1length+fq2length)/genomesize)
+#     print('Standard Sequencing Depth is {}X'.format(std_depth))
+#     return std_depth
 def run_step2(fq1file,fq2file,genomefile):
     '''
     用二代reads的总长度/genome的大小得出标准测序深度
@@ -144,6 +157,7 @@ def ctg_phasing_base_depth(fasta_dict,depthcountfile,std_depth,Ploidy,result):
     :param result:
     :return:
     '''
+    # ctg_file   = fasta_dict
     depth_file = open(depthcountfile,'r')
     std_depth  = int(std_depth)
     ploidy = int(Ploidy)
@@ -177,14 +191,22 @@ def main(args):
     depth_count(depthfile,depthfileout)
     Fontcolor.Log_output('-' * 10 + 'step 1 completed !' + '-' * 10)
 
-    fq1_file = args.fastq1 #二代的reads1
-    fq2_file = args.fastq2 #二代的reads1
-    genome_file = args.genome #三代的基因组文件
-    Fontcolor.Log_output('-' * 10 + 'step 2 beginning !' + '-' * 10)
-    std_depth = run_step2(fq1_file,fq2_file,genome_file)
-    Fontcolor.Log_output('-' * 10 + 'step 2 completed !' + '-' * 10)
+    if not args.stdth:
+        fq1_file = args.fastq1 #二代的reads1
+        fq2_file = args.fastq2 #二代的reads1
+        genome_file = args.genome #三代的基因组文件
+        Fontcolor.Log_output('-' * 10 + 'step 2 beginning !' + '-' * 10)
+        std_depth = run_step2(fq1_file,fq2_file,genome_file)
+        Fontcolor.Log_output('-' * 10 + 'step 2 completed !' + '-' * 10)
+    else:
+        Fontcolor.Log_output('-' * 10 + 'step 2 beginning !' + '-' * 10)
+        std_depth = args.stdth
+        Fontcolor.Tips_output('Input std depth is {}X'.format(std_depth))
+        Fontcolor.Log_output('-' * 10 + 'step 2 completed !' + '-' * 10)
+
     Ploidy = args.ploidy
     output = args.output
+    genome_file = args.genome
     Fontcolor.Log_output('-' * 10 + 'step 3 beginning !' + '-' * 10)
     fasta_dict = Fa2dict(genome_file)
     ctg_phasing_base_depth(fasta_dict,depthfileout,std_depth,Ploidy,output)
@@ -192,17 +214,19 @@ def main(args):
 
 
 if __name__ == '__main__':
+    # depth_count(sys.argv[1],sys.argv[2])
     parser = argparse.ArgumentParser(
         prog=sys.argv[0],
         formatter_class=argparse.RawTextHelpFormatter,
         description='''Polyploid genome phasing:Use Second-generation sequencing reads mapping Third-generation sequencing genome''',
-        usage="python {} -d depth -o depthout -f1 fq1 -f2 fq2 -g genome -p ploidy -O output".format(sys.argv[0]),
+        usage="python {} -d depth -o depthout -f1 fq1 -f2 fq2 -g genome -p ploidy -O output\n\t\tor\n\t\tpython {} -d depth -o depthout -s std_depth -g genome -p ploidy -O output".format(sys.argv[0],sys.argv[0]),
         epilog='author:\t{0}\nmail:\t{1}\ndate:\t{2}\nversion:\t{3}'.format(__author__, __mail__, __date__,
                                                                             __version__))
     parser.add_argument('-d', '--depth', required=True, help='Input samtools depth outputfile')
     parser.add_argument('-o', '--depthout', required=True, help='Output ctg depth result(.tab)')
-    parser.add_argument('-f1', '--fastq1', required=True, help='Input sample1 file(fastq1.gz)')
-    parser.add_argument('-f2', '--fastq2', required=True, help='Input sample2 file(fastq2.gz)')
+    parser.add_argument('-f1', '--fastq1', required=None, help='Input sample1 file(fastq1.gz)')
+    parser.add_argument('-f2', '--fastq2', required=None, help='Input sample2 file(fastq2.gz)')
+    parser.add_argument('-s', '--stdth', required=None, help='Input the standard depth of known sequencing data, which is equal to the sequencing data/genome size(bp)')
     parser.add_argument('-g', '--genome', required=True, help='Input Thr Sequencing assmbly genome(.fasta)')
     parser.add_argument('-p', '--ploidy', required=True, help='Input Ploidy of species(eg:8)')
     parser.add_argument('-O', '--output', required=True, help='Output the fasta sequence of the contig that only needs to be phased(.fasta)')
