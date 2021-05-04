afe89159bdb9b79bc94b7f5089e3f596add4d744
Panpyhr <51531508+valderpan@users.noreply.github.com>
2021-05-04 15:23:00 +0800

    Update muscle2phylip.py

--- Comparative_genomics/muscle2phylip.py
+++ Comparative_genomics/muscle2phylip.py
@@ -89,7 +89,7 @@ if __name__ == '__main__':
         usage="python {} -p msa_path -s ID1 ID2 ID3..\n\nNote:ID is the ID of the gene in the genome of each species!!!\nGenerated file: SingleCopy.pep.phylip".format(sys.argv[0]),
         epilog= 'author:\t{0}\nmail:\t{1}\ndate:\t{2}\nversion:\t{3}'.format(__author__,__mail__,__date__,__version__))
     parser.add_argument('-p','--path',required=True,help='Input the path where the msa file is located')
-    parser.add_argument('-t','--type',required=True,choices=['pep','cds'],help='Input the path where the msa file is located')
+    parser.add_argument('-t','--type',required=True,choices=['pep','cds'],help='Specify the type of input sequence')
     parser.add_argument('-s','--species',required=True,nargs='+',help='Input the gene ID prefix for each species genome')
     args = parser.parse_args()
     main(args)
