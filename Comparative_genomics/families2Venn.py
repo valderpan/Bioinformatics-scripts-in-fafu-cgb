b6ba9f2b03e2798c3d6f4d3df7c809c8d16f61cb
Panpyhr <51531508+valderpan@users.noreply.github.com>
2021-08-18 15:10:28 +0800

    Update families2Venn.py

--- Comparative_genomics/families2Venn.py
+++ Comparative_genomics/families2Venn.py
@@ -10,19 +10,20 @@ Gene family clustering based on orthofinder results
 
 __author__ = Haoran Pan
 __mail__ = haoran_pan@qq.com
-__date__ = 20210418
-__version__ = v4.0
+__date__ = 20210818
+__version__ = v4.1
 '''
 
-import pandas as pd
 import sys
 import re
+import pandas as pd
+
 
 def countfamilies(file):
     df = pd.read_table(file,sep='\t')
     col_names = [col for col in df][1:-1]
     for colnames in col_names:
-        species_name = re.findall('([0-9a-zA-Z\_\.]+)\.pep',colnames)[0]
+        species_name = re.findall('([0-9a-zA-Z\_\.\-]+)\.pep',colnames)[0]
         families_OG = df[df[colnames] !=0].iloc[:,0].tolist()
         with open(species_name+'.group.txt','w') as f:
             for OG in families_OG:
@@ -31,7 +32,8 @@ def countfamilies(file):
 
 if __name__ == '__main__':
     from optparse import OptionParser
-
+    from rich.traceback import install
+    install()
     p = OptionParser(__doc__)
     opts, args = p.parse_args()
 
