ec4a9f519ee185d36e7a4adbd71fb6ef557d4579
Panpyhr <51531508+valderpan@users.noreply.github.com>
2021-11-10 10:59:39 +0800

    Update extract_fasta.py

--- scripts_used_frequently/extract_fasta.py
+++ scripts_used_frequently/extract_fasta.py
@@ -7,12 +7,13 @@
 import sys
 import argparse
 import filter_fasta as ff
+from rich import print
 from rich.traceback import install
 
 __author__ = 'Haoran Pan'
 __mail__ = 'panpyhr@gmail.com'
-__date__ = '20210824'
-__version__ = 'v1.0'
+__date__ = '20211110'
+__version__ = 'v1.3'
 
 
 def read_seqID(file):
@@ -21,15 +22,24 @@ def read_seqID(file):
         lines = (line.strip() for line in f)
         for line in lines:
             ID.append(line)
+    print('{} seqs are obtained'.format(len(ID)))
     return ID
 
 
 def ExtractandOut_seq(fastaD,ID,output):
+    nomatch = []
+    match = 0
     with open(output,'w') as w:
         for key in fastaD.keys():
             if key in ID:
+                match += 1
                 w.write(">{}\n".format(key))
                 w.write(str(fastaD[key])+'\n')
+            else:
+                nomatch.append(key)
+    print('{} seqs are matched'.format(match))
+    if len(nomatch)>0:
+        print('{} seqs did not match with {},{},{},...'.format(len(nomatch),nomatch[0],nomatch[1],nomatch[2]))
 
 
 def main(args):
@@ -47,7 +57,7 @@ if __name__ == '__main__':
         prog=sys.argv[0],
         formatter_class=argparse.RawTextHelpFormatter,
         description='''Extract query seq by header''',
-        usage="python {} -f fasta -m mode -k keywords -o output.fasta ".format(sys.argv[0]),
+        usage="python {} -f fasta -q queryid.txt -o output.fasta ".format(sys.argv[0]),
         epilog='author:\t{0}\nmail:\t{1}\ndate:\t{2}\nversion:\t{3}'.format(__author__,
                                                                             __mail__, __date__, __version__))
     parser.add_argument('-f', '--fasta', required=True, help='Input fasta')
