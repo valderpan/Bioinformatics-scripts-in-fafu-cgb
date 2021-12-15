c3bdb64ab79f17305ce2e1779759176bc4b73898
Panpyhr <51531508+valderpan@users.noreply.github.com>
2021-12-15 10:12:34 +0800

    Update fa2dict_Bio.py

--- Assembly/fa2dict_Bio.py
+++ Assembly/fa2dict_Bio.py
@@ -2,17 +2,10 @@
 # -*- encoding:utf-8 -*-
 # @Author : Haoran Pan
 
-# from Bio import SeqIO
-#
-# def Fa2dict_Bio(fasta_file):
-#     fasta_dict = {}
-#     for seq in SeqIO.parse(fasta_file,'fasta'):
-#         fasta_dict[seq.id] = seq.seq
-#     return fasta_dict
+from Bio import SeqIO
 
-
-Dict = {'a':'aaa','b':'bbbbbbbb','c':'cccc'}
-print(list(enumerate(sorted(Dict.items(), key=lambda x: len(x[1])))))
-print(list(enumerate(sorted(Dict.items(), key=lambda x: len(x[1]))))[0][1][1])
-print(list(enumerate(sorted(Dict.items(), key=lambda x: len(x[1]))))[-1][1][1])
-print(len(list(enumerate(sorted(Dict.items(), key=lambda x: len(x[1]))))[0][1][0]))
+def Fa2dict_Bio(fasta_file):
+    fasta_dict = {}
+    for seq in SeqIO.parse(fasta_file,'fasta'):
+        fasta_dict[seq.id] = seq.seq
+    return fasta_dict
