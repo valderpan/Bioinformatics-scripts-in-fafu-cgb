7d3506f4173b587625a45fee090b40fb084d96bb
Purrhr <51531508+valderpan@users.noreply.github.com>
2022-04-02 13:29:18 +0800

    Update TEstats.py

--- Repeat_TE/TEstats.py
+++ Repeat_TE/TEstats.py
@@ -5,18 +5,21 @@
 
 
 '''
-%prog <RM out file>
+%prog <RM out file> <genome_fasta>
 
-将RepeatMasker预测出来的基因组重复序列成立为表发级别的表格形式
+Counting genomic repeat sequences predicted by RepeatMasker
+
+>>> python %prog ZG.v20211208.genome.fasta.out ZG.v20211208.genome.fasta
 
 __author__ = 'Haoran Pan'
 __mail__ = 'panpyhr@gmail.com'
-__date__ = '20211213'
-__version__ = 'v1.0'
+__date__ = '20220402'
+__version__ = 'v1.2'
 '''
 
 
 import sys
+import fa2dict_Bio as fdB
 from rich import print
 
 
@@ -44,10 +47,22 @@ def parse_RMout(file):
     return family_lenD,family_numD
 
 
-def output_result(family_lenD,family_numD):
+def get_genomesize(fastaD):
+    num = 0
+    for key in fastaD.keys():
+        num += len(fastaD[key])
+    return num
+
+
+def output_result(family_lenD,family_numD,genome_size):
+    repeat_num = 0
     for key in family_lenD.keys():
-        print('{}:\n\tNumber: {}\n\tLength (bp): {}\n\tPercent (%): {}'.format(key,family_numD[key],
-                                                           family_lenD[key],round(family_lenD[key]/6577007109*100,3)))
+        repeat_num += family_lenD[key]
+    for key in family_lenD.keys():
+        print('{}:\n\tNumber: {}\n\tLength (bp): {}\n\tPercent of genome(%): {}\n\tPecentage of repeat(%): {}'.format(
+                                                           key,family_numD[key],family_lenD[key],
+                                                           round(family_lenD[key]/int(genome_size)*100,3),
+                                                           round(family_lenD[key]/repeat_num*100,3)))
 
 
 if __name__ == '__main__':
@@ -56,10 +71,14 @@ if __name__ == '__main__':
     install()
     p = OptionParser(__doc__)
     opts, args = p.parse_args()
-    if len(args) == 1:
+    if len(args) == 2:
         out_file = args[0]
+        genome_fasta = args[1]
         lenD, numD = parse_RMout(out_file)
-        output_result(lenD, numD)
+        fastaD = fdB.Fa2dict_Bio(genome_fasta)
+        genome_size = get_genomesize(fastaD)
+        print('Genome size : {}bp'.format(genome_size))
+        output_result(lenD,numD,genome_size)
     else:
         print('Usage:')
         print('\tpython {0} <RM.out>'.format(sys.argv[0]))
